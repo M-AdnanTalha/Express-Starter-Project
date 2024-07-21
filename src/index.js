57200e4b25b0225a3770ff5e378a1e6eb67c01f9
Mohd Adnan Talha <adnan.ad7077@gmail.com>
2024-07-22 02:14:07 +0530

    ExpressStarter Complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const ServerConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
 
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-app.use(bodyParser.urlencoded());
+//CHECK FROM HERE 1:23:00
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended:true}));
+
+app.post('/ping',(req,res)=>{
+    console.log(req.body);
+    return res.json({message:"pong"});
+})
 
 app.listen(ServerConfig.PORT , async ()=>{
     await connectDB();
